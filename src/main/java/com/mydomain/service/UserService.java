8a9bab54c7b3c4b559cba141eaa676fa1df824c8
nandishkb <encodefox@gmail.com>
2016-04-07 14:11:22 +0530

    New modification for managing hibernate

--- src/main/java/com/mydomain/service/UserService.java
+++ src/main/java/com/mydomain/service/UserService.java
@@ -1,13 +1,21 @@
 package com.mydomain.service;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
 import com.mydomain.model.Users;
 
@@ -15,11 +23,80 @@ import com.mydomain.model.Users;
 public class UserService {
 
 	@GET
-	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public List<Users> getUsers() {
-		Users user = new Users("Nandish", "nandibal@cisco.com", "#####23", new Date(), 30, "KA");
-		List<Users> users = new ArrayList<Users>();
-		users.add(user);
-		return users;
+	@Path("/{param}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public Users getUser(@PathParam("param") Integer id) {
+		Session ses = HibernateUtil.currentSession();
+		try {
+			Criteria crit =  ses.createCriteria(Users.class);
+			crit.add(Restrictions.idEq(id));
+			Users u = (Users)crit.uniqueResult();
+			return u;
+		} finally {
+			HibernateUtil.closeSession();
+		}
 	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public List<Users> getUsers() {
+		Session ses = HibernateUtil.currentSession();
+		try {
+			return ses.createCriteria(Users.class).list();
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	//public void createUser(@FormParam("name") String name,@FormParam("age") Integer age,@FormParam("emailId") String emailId){
+	public void createUser(Users u){
+		System.out.println("Creating user: "+u.getName()+" Age: "+u.getAge());
+		Session ses = HibernateUtil.currentSession();
+		try {
+			Transaction tx = ses.beginTransaction();
+			ses.save(u);
+			tx.commit();
+		}finally{
+			HibernateUtil.closeSession();
+		}
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	//public void updateUser(@FormParam("id") Integer id, @FormParam("name") String name,@FormParam("age") Integer age,@FormParam("emailId") String emailId){
+	public void updateUser(Users u){
+		System.out.println("Updating user: "+u.getName());
+		Session ses = HibernateUtil.currentSession();
+		try {
+			Transaction tx = ses.beginTransaction();
+			ses.update(u);
+			tx.commit();
+		}finally{
+			HibernateUtil.closeSession();
+		}
+	}
+	
+	@DELETE
+	@Path("/{param}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public boolean deleteUser(@PathParam("param") Integer id) {
+		System.out.println("Deleting user: "+id);
+		Session ses = HibernateUtil.currentSession();
+		try {
+			Transaction tx = ses.beginTransaction();
+			Users u = (Users) ses.load(Users.class, id);
+			ses.delete(u);
+			tx.commit();
+			return true;
+		} finally {
+			HibernateUtil.closeSession();
+		}
+	}
+	
 }
+
+
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,4 +1,12 @@
 <html>
+<head>
+	<script src="scripts/jquery-2.1.3.js"></script>
+	<script>
+	$(document).ready(function() {
+		alert("Document Ready");
+	});
+	</script>
+</head>
 <body>
 <h2>Hello World!</h2>
 </body>
